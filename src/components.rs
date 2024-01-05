6d2612ea3520d1bc49055779c784f0a95eea46c9
AJV <alexander.valarus@gmail.com>
2024-01-05 15:12:23 -0500

    Refactored components and made player ship movement work using rotate/forward instead of direct left/right/up/down

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,14 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub struct Player {
-    pub speed: f32
+pub struct Ship {
+    pub speed: f32,
+    pub angle: f32,
+    pub turn_speed: f32,
 }
+
+#[derive(Component)]
+pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy{}
--- src/player_sys.rs
+++ src/player_sys.rs
@@ -14,36 +14,39 @@ pub fn spawn_player(
             texture: asset_server.load("sprites/ships/playerShip1_blue.png"),
             ..default()
         },
-        Player {speed: 500.0},
+        Player {},
+        Ship {speed: 400.0, angle: f32::to_radians(90.0), turn_speed: f32::to_radians(1.25)}
     ));
 }
 
 pub fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<(&Player, &mut Transform), With<Player>>,
+    mut player_query: Query<(&mut Ship, &mut Transform), With<Player>>,
     time: Res<Time>,
 ) {
-    if let Ok((player, mut transform)) = player_query.get_single_mut() {
+    if let Ok((mut ship, mut transform)) = player_query.get_single_mut() {
 
         let mut direction = Vec3::ZERO;
 
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            let move_dir = transform.up() * ship.speed * time.delta_seconds();
+            transform.translation += move_dir;
+        }
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-1.0, 0.0, 0.0)
+            // Using angles, so if turning left hits 360.0 degrees, it wraps around to 0.0.
+            transform.rotate_z(ship.turn_speed);
+            println!("Angle: {:?}", transform.rotation);
         }
         if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1.0, 0.0, 0.0)
-        }
-        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 1.0, 0.0)
-        }
-        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0.0, -1.0, 0.0)
+            // Using angles, so if turning right hits 0.0 degrees, it wraps around to 360.0.
+            transform.rotate_z(-ship.turn_speed);
+            println!("Angle: {:?}", transform.rotation);
         }
 
         if direction.length() > 0.0 {
             direction = direction.normalize();
         }
 
-        transform.translation += direction * player.speed * time.delta_seconds();
+        transform.translation += direction * ship.speed * time.delta_seconds();
     }
 }
