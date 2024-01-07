13169fedb42ac4ab431c8dd4bc8e1da118f00777
AJV <alexander.valarus@gmail.com>
2024-01-06 22:20:37 -0500

    Implemented a very basic screen-wrap system, so that play wraps around the screen instead of being confined within the screen.

--- src/camera_sys.rs
+++ src/camera_sys.rs
@@ -1,4 +1,5 @@
 // use bevy::core_pipeline::bloom::BloomSettings;
+use crate::components::Player;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
@@ -22,4 +23,69 @@ pub fn spawn_camera(
         },
         // BloomSettings::OLD_SCHOOL,
     ));
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = 32.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        // Bound the player x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        // Bound the player y position
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation;
+    }
+}
+
+pub fn wrap_player_location(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let x_min = 0.0;
+        let x_max = window.width();
+        let y_min = 0.0;
+        let y_max = window.height();
+
+        let mut translation = player_transform.translation;
+
+        // Bound the player x position
+        if translation.x < x_min {
+            translation.x = x_max;
+        } else if translation.x > x_max {
+            translation.x = x_min;
+        }
+
+        // Bound the player y position
+        if translation.y < y_min {
+            translation.y = y_max;
+        } else if translation.y > y_max {
+            translation.y = y_min;
+        }
+
+        player_transform.translation = translation;
+    }
 }
